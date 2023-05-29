d87dacf54c16e51bbc5dde639196dc36d87e0d21
0derb0 <ni3132887@gmail.com>
2023-05-29 23:08:00 +0300

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
 let openorclose = 'flex';
+let autoOrZero = 'auto'
 
 function senseInDesighButton(){
-    let a = document.getElementById('open_cards').style.display = openorclose;
+    document.getElementById('open_cards').style.display = openorclose;
+    document.getElementById('open_cards').style.maxHeight = autoOrZero;
     if (openorclose == 'flex') {
         openorclose = 'none'
     } else if (openorclose == 'none') {
         openorclose = 'flex'
+    }
+    if (autoOrZero == 'auto') {
+        autoOrZero = '0'
+    }else if (autoOrZero == '0') {
+        autoOrZero = 'auto'
     }
 }
